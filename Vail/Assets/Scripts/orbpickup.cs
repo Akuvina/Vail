8c345b796cfa92019ce9cf7404f71e5cb5eaa4cc
Mika-Wolf <alphawolf7898@gmail.com>
2015-10-05 19:51:41 -0400

    Background Change

---
+++ Vail/Assets/Scripts/orbpickup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orbpickup : MonoBehaviour {
+	public Color color1 = Color.red;
+
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if (other.gameObject.CompareTag ("Player"))
+			Destroy (gameObject);
+		Camera.main.backgroundColor = color1;
+	}
+
+
+}
